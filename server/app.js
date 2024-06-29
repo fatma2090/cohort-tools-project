041e7b2944d009edbeb98e29219d40dc065536e8
Michael Nguyen <michael.nguyen@live.nl>
2024-06-29 16:21:20 +0200

    All tasks day 1

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
-
+const cohorts = require("./cohorts.json");
+const students = require("./students.json");
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
@@ -17,6 +19,11 @@ const app = express();
 // ...
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: ['http://localhost:5173'], 
+  })
+);
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -27,6 +34,12 @@ app.use(cookieParser());
 // ...
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
+});
+app.get("/api/cohorts", (req, res) => {
+  res.json(cohorts);
+});
+app.get("/api/students", (req, res) => {
+  res.json(students);
 });
 
 
